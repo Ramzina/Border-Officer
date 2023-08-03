6a2218304ee8c4e0bc68b54f0eb6d1d8f7f37c24
Ramzina <nolandovader@gmail.com>
2023-08-03 20:09:11 +0100

    Added main files
    Added main files and folders, imperative to bot function

---
+++ commands/General.py
@@ -0,0 +1,233 @@
+from discord import Color
+import discord
+from discord import ui
+from discord.interactions import Interaction
+import python_weather
+import random
+from discord.ext.commands import (
+	CommandNotFound,
+	CommandOnCooldown,
+	MissingPermissions,
+	MissingRequiredArgument,
+)
+from datetime import datetime
+from discord.app_commands import AppCommandError
+import aiosqlite
+from discord.ext import commands, tasks
+from discord import app_commands
+import random
+
+
+class General(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+        
+
+####################################################################################################################################
+#################################################################################################################################### ECHO START
+####################################################################################################################################
+
+	@app_commands.command(name="echo", description="The bot says what you pass. (Backtraceable)")
+	@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+	async def echo(self, interaction: discord.Interaction, message: str) -> None:
+		print("[Echo] has just been executed")
+		await interaction.response.defer(thinking=True)
+
+		db = await aiosqlite.connect("config.db")
+
+		await db.execute(
+			"""CREATE TABLE IF NOT EXISTS logs(guild_name STRING, guild_id INTEGER, channel_id INTEGER)"""
+		)
+		res = await db.execute(
+			f"""SELECT * FROM logs WHERE guild_id = {interaction.guild.id}"""
+		)
+		row = await res.fetchone()
+		if row is None:
+			await interaction.followup.send(f'{interaction.user.mention}, To setup {self.bot.user}\'s commands properly, run /logs to set a commands log channel')
+
+		res = await db.execute(
+			f"""SELECT channel_id FROM logs WHERE guild_id = {interaction.guild.id}"""
+		)
+		channel = await res.fetchone()
+		logs = channel[0]
+
+		channel = self.bot.get_channel(logs)
+		await interaction.followup.send(f"{message}")
+		embed = discord.Embed(
+			title="! **ECHO** !", description=f"- 🧍 User: {interaction.user.mention}\n- 🪪 User name: {interaction.user}\n- 🪪 User Id: `{interaction.user.id}`\n- 🖊️ Message: {message}",timestamp=datetime.utcnow(),color=Color.from_rgb(27, 152, 250)
+		)
+		if logs is not None:
+			await channel.send(
+				embed=embed.set_thumbnail(url=interaction.user.avatar.url)
+			)
+		else:
+			pass
+
+	@echo.error
+	async def on_echo_error(
+		self, interaction: discord.Interaction, error: AppCommandError
+	):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			await interaction.response.send_message(content=str(error), ephemeral=True)
+
+####################################################################################################################################
+#################################################################################################################################### ECHO END, KYS START
+####################################################################################################################################
+
+	@app_commands.command(
+		name="kys", description="Tells the specified user to kill themself"
+	)
+	@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+	async def kys(
+		self, interaction: discord.Interaction, member: discord.Member
+	) -> None:
+		print("[Kys] has just been executed")
+		await interaction.response.defer(ephemeral=True, thinking=True)
+
+		if member.mention == interaction.user.mention:
+			embed = discord.Embed(title="! Error !",timestamp=datetime.utcnow(), color=Color.from_rgb(27, 152, 250))
+			embed.add_field(name="Command:", value=f"`{interaction.command}`")
+			embed.add_field(
+				name="Error:", value="`Passed member is the same as the message author`"
+			)
+			await interaction.followup.send(
+				embed=embed.set_thumbnail(url=interaction.user.avatar_url)
+			)
+			return
+		embed = discord.Embed(
+			title="! **• KYS ALERT •** !",
+			description=f"\n- Alert {member.mention}, {interaction.user.mention} thinks you should impale yourself on a railing!\n:O\n",
+			timestamp=datetime.utcnow(),
+			color=Color.from_rgb(27, 152, 250),
+		)
+
+		embed.set_footer(text=f"Border hopping hispanic")
+		await interaction.response.send_message(
+			content=f"||{member.mention}||",
+			embed=embed.set_thumbnail(url=interaction.user.avatar.url),
+		)
+
+	@kys.error
+	async def on_kys_error(
+		self, interaction: discord.Interaction, error: AppCommandError
+	):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			await interaction.response.send_message(content=str(error), ephemeral=True)
+
+
+####################################################################################################################################
+#################################################################################################################################### KYS END, PING START
+####################################################################################################################################
+
+	@app_commands.command(name="ping", description="Sends the bots latency in a cool embed!")
+	@app_commands.checks.cooldown(1, 5, key=lambda i: (i.guild_id, i.user.id))
+	async def ping(self, interaction: discord.Interaction):
+		print("[Ping] has just been executed")
+		await interaction.response.defer(ephemeral=True)
+
+		if round(self.bot.latency * 1000) < 150:
+			COLOR=Color.green()
+			result = 'Fast'
+		elif round(self.bot.latency * 1000) < 200:
+			COLOR=Color.yellow()
+			result = 'Average'
+		elif round(self.bot.latency * 1000) > 300:
+			COLOR=Color.red()
+			result = 'Slow'
+
+		embed = discord.Embed(
+			title="**Ping**",
+			description=f"> Latency: {round(self.bot.latency * 1000)}ms 🏓\n> Speed: {result}",
+			color=COLOR,
+		)
+
+		await interaction.followup.send(
+			embed=embed, ephemeral=True
+		)
+
+	@ping.error
+	async def on_ping_error(
+		self, interaction: discord.Interaction, error: AppCommandError
+	):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			await interaction.response.send_message(content=str(error), ephemeral=True)
+
+####################################################################################################################################
+#################################################################################################################################### PING END,  START
+####################################################################################################################################
+
+	@app_commands.command(
+		name="random", description="Gives a random number between the two you pass."
+	)
+	@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
+	async def random(
+		self, interaction: discord.Interaction, num1: int = 1, num2: int = 100
+	):
+		await interaction.response.defer(ephemeral=True, thinking=True)
+		print("[Random] has just been executed")
+
+		embed = discord.Embed(
+			title="**Random**",
+			description=f"> Number: {random.randint(num1, num2)}",
+			color=Color.green(),
+		)
+		await interaction.followup.send(
+			embed=embed, ephemeral=True
+		)
+
+	@random.error
+	async def on_random_error(
+		self, interaction: discord.Interaction, error: AppCommandError
+	):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			await interaction.response.send_message(content=str(error), ephemeral=True)
+
+####################################################################################################################################
+#################################################################################################################################### RANDOM END, WEATHER START
+####################################################################################################################################
+
+	@app_commands.command(name='weather', description='Gets the forecast in the selected city.')
+	@app_commands.checks.cooldown(1,15, key=lambda i: (i.guild.id, i.user.id))
+	async def weather(self, interaction:discord.Interaction, city:str):
+		await interaction.response.defer(ephemeral=True, thinking=True)
+		print('[Weather] has just been executed.')
+		async with python_weather.Client(unit=python_weather.METRIC) as client:
+			weather = await client.get(city)
+			embed = discord.Embed(title=f'**Weather in {city.capitalize()}**', description=f"> ☁️ Weather: {weather.current.description}\n> 🌥️ Temperature: {weather.current.temperature}C°\n> 💦 Humidity: {weather.current.humidity}%\n> 🚩 Wind speed: {weather.current.wind_speed} Km/h\n> 🚩 Wind direction: {weather.current.wind_direction}",
+			color=Color.blue())
+			await interaction.followup.send(embed=embed)
+	@weather.error
+	async def on_weather_error(
+		self, interaction: discord.Interaction, error: AppCommandError
+	):
+		if isinstance(error, app_commands.CommandOnCooldown):
+			await interaction.response.send_message(content=str(error), ephemeral=True)
+
+
+####################################################################################################################################
+#################################################################################################################################### WEATHER END, 8BALL START
+####################################################################################################################################
+
+	@app_commands.command(name='8ball', description='Ask the holy 8ball a question!')
+	@app_commands.checks.cooldown(1,15, key=lambda i: (i.guild.id, i.user.id))
+	async def eightball(self,interaction:discord.Interaction, question:str):
+		await interaction.response.defer(ephemeral=True, thinking=True)
+		print('[8ball] has just been executed.')
+
+		possible_answers = ['Yes.', 'No.', 'Possibly.', 'Maybe.', 'The stars tell me yes.', 'The stars tell me no.', 'The stars are unsure.', 'The stars don\'t know yet.']
+
+		answer = random.choice(possible_answers)
+
+		embed = discord.Embed(title='**The magic 8ball**', description=f'- {interaction.user.mention} asked: "{question}"\n- Magic 8ball says: "{answer}"', color=Color.from_rgb(27, 152, 250))
+
+		await interaction.followup.send(embed=embed.set_thumbnail(url=interaction.user.avatar.url))
+
+####################################################################################################################################
+#################################################################################################################################### 8BALL END
+####################################################################################################################################
+
+
+
+async def setup(bot):
+	await bot.add_cog(General(bot))
---
+++ commands/Ticketing.py
@@ -0,0 +1,275 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord import Color
+from datetime import datetime
+import aiosqlite
+import os
+import asyncio
+from discord.ui import Button,button, View
+from random import randint
+
+class CloseButton(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+
+    @button(label='Close ticket', style=discord.ButtonStyle.red,custom_id="closeticket", emoji="🔒")
+    async def close(self, interaction:discord.Interaction, button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        view = CreateButton()
+        view.message = await interaction.original_response()
+
+        await view.message.edit(view=None)
+
+        global ticket_creator
+        ticket_creator = discord.utils.get(interaction.guild.members, id=int(interaction.channel.topic))
+        await interaction.channel.set_permissions(ticket_creator, send_messages=False,read_message_history=False)
+
+        await interaction.channel.send("Closing ticket...")
+        await asyncio.sleep(2)
+
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False, view_channel = False),
+            ticket_creator: discord.PermissionOverwrite(read_messages=False, send_messages=False, view_channel=False),
+            interaction.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        }
+
+        await interaction.channel.edit(overwrites=overwrites,name=f"Closed - {ticket_creator}", topic=f"Closed - {ticket_creator.id}")
+        global msg
+        msg = await interaction.channel.send(embed=discord.Embed(
+            description=f'Ticket closed by {interaction.user.mention}. 🔒',
+            color=Color.red()
+        ),
+        view=TrashOpenAndTranscriptButton())
+
+class CreateButton(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @button(label='Create Ticket', style=discord.ButtonStyle.blurple, emoji='🎫', custom_id='ticketopen')
+    async def ticket(self, interaction:discord.Interaction, button: Button):
+        await interaction.response.defer(ephemeral=True)
+        global opener
+        opener = interaction.user
+        db = await aiosqlite.connect("ticketing.db")
+
+        cur = await db.execute("""SELECT ticket_banned FROM ticketing WHERE guild_id = ?""", (interaction.guild.id, ))
+        ticket_banned_role = await cur.fetchone()
+        
+        t_banned = discord.utils.get(interaction.guild.roles, id=ticket_banned_role[0])
+        if t_banned in interaction.user.roles:
+            await interaction.followup.send(embed=discord.Embed(
+                description=f"{interaction.user.mention}, you are banned from making tickets.",
+                color=Color.red()
+            ),ephemeral=True)
+            return
+
+        cur = await db.execute("""SELECT ticket_category_id FROM ticketing WHERE guild_id = ?""", (interaction.guild.id, ))
+        Category = await cur.fetchone()
+        T_category = Category[0]
+
+        category = discord.utils.get(interaction.guild.categories, id = T_category)
+        for ch in category.text_channels:
+            if ch.topic == f"{interaction.user.id}":
+                await interaction.followup.send("You already have a ticket in {0}".format(ch.mention), ephemeral=True)
+                return
+
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False, view_channel=False),
+            interaction.user: discord.PermissionOverwrite(read_messages=True, send_messages=True, view_channel = True),
+            interaction.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        }
+        channel = await category.create_text_channel(
+            name=f"{interaction.user}s ticket",
+            topic=f'{interaction.user.id}',
+            overwrites = overwrites
+        )
+        global meeswage
+        meeswage = await channel.send(
+            embed=discord.Embed(
+            title=f"{interaction.user}'s Ban appeal ticket.",
+            timestamp=datetime.utcnow(),
+            description='Why you were banned (This will be checked),\nWhy you think you should be unbanned,\nWhat would you change to not be banned again.\n \nPlease don\'t spam support is being as fast as they can.',
+            color=Color.green(),
+            ),
+        view=CloseButton())
+        await interaction.followup.send(f'<#{channel.id}> created!', ephemeral=True)
+
+class TrashOpenAndTranscriptButton(View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @button(label="Open ticket", style=discord.ButtonStyle.green, emoji="🔓", custom_id="openticket")
+    async def open(self, interaction:discord.Interaction, button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        await msg.edit(view=None)
+
+        await interaction.edit_original_response(view=None)
+
+        await interaction.channel.send("Opening ticket...")
+        await asyncio.sleep(3)
+        await interaction.followup.send(embed=discord.Embed(description=f"Ticket opened by {interaction.user.mention}. 🔓",color=Color.green()))
+
+        ticket_creator = discord.utils.get(interaction.guild.members, id=int(interaction.channel.topic.replace('Closed - ', '')))
+
+
+        overwrites = {
+            interaction.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            ticket_creator: discord.PermissionOverwrite(read_messages=True,view_channel=True, send_messages=True),
+            interaction.guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True)
+        }
+        await interaction.channel.edit(overwrites=overwrites,name=f'{ticket_creator}s ticket', topic=f'{ticket_creator.id}')
+
+        await meeswage.edit(view=CloseButton())
+
+
+    
+    @button(label="Delete ticket", style=discord.ButtonStyle.red, emoji="⛔", custom_id="deleteticket")
+    async def delete(self, interaction:discord.Interaction, button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        await msg.edit(view=None)
+
+        await interaction.channel.send("Deleting ticket...")
+        await asyncio.sleep(3)
+        await interaction.followup.send(embed=discord.Embed(description=f"Ticket delted by {interaction.user.mention}. ⛔",color=Color.red()))
+        await asyncio.sleep(1)
+
+        db = await aiosqlite.connect("ticketing.db")
+        cur = await db.execute("""SELECT auto_transcript FROM ticketing WHERE guild_id = ?""", (interaction.guild.id, ))
+
+        auto_transcript = await cur.fetchone()
+        if auto_transcript[0] == 'True':
+            message_list = []
+            async for message in interaction.channel.history(limit=None):
+                message_list.append(message)
+
+            with open(f"{interaction.channel.id}_{interaction.user}.txt", "a" ,encoding="utf=8") as f:
+                        for message in reversed(message_list):
+                            if message.embeds:
+                                if message.embeds[0].title:
+                                    f.write(f"TITLE - {message.embeds[0].title}\n")
+                                if message.embeds[0].description:
+                                    f.write(f"DESCRIPTION - {message.embeds[0].description}\n")
+                                if len(message.embeds[0].fields) > 0:
+                                    for field in message.embeds[0].fields:
+                                        f.write(f"FIELD - {field.name} - {field.value}\n")
+                                if message.embeds[0].footer:
+                                    f.write(f"FOOTER - {message.embeds[0].footer.text}\n")
+                                if message.embeds[0].image:
+                                    f.write(f"IMAGE - {message.embeds[0].image.url}\n")
+                                if message.embeds[0].thumbnail:
+                                    f.write(f"THUMBNAIL - {message.embeds[0].thumbnail.url}\n")
+                                if message.embeds[0].author:
+                                    f.write(f"AUTHOR - {message.embeds[0].author.name}\n")
+                                if message.embeds[0].url:
+                                    f.write(f"LINK - {message.embeds[0].url}\n")
+                                    f.write(f"LINK - {message.jump_url}\n\n")
+                                continue
+                            f.write(f"{message.author}: {message.content}\n")
+
+            db = await aiosqlite.connect("config.db")
+            cur = await db.execute("""SELECT channel_id FROM logs WHERE guild_id = ?""", (interaction.guild.id, ))
+
+            channel = await cur.fetchone()
+            chan = discord.utils.get(interaction.guild.text_channels, id=channel[0])
+            if channel is None:
+                await interaction.channel.send(f'Error sending transcript, run /logs and choose you bot logs channel.')
+                await asyncio.sleep(3)
+            if channel is not None:
+                await chan.send(file=discord.File(f"{interaction.channel.id}_{interaction.user}.txt"))
+                os.remove(f"{interaction.channel.id}_{interaction.user}.txt")
+
+        await interaction.channel.delete()
+
+    @button(label='Send transcript', style=discord.ButtonStyle.blurple, emoji="📑", custom_id='transcriptticket')
+    async def transcript(self, interaction:discord.Interaction, button: Button):
+        await interaction.response.defer(ephemeral=True)
+
+        await interaction.followup.send('Sending transcript...')
+
+        db = await aiosqlite.connect("config.db")
+        cur = await db.execute("""SELECT * FROM logs WHERE guild_id=?""", (interaction.guild.id, ))
+
+        row = await cur.fetchone()
+        if row is None:
+            await interaction.channel.send(f"To setup {self.bot.user}\'s commands properly, run /help and choose the BotSetup section")
+            return
+        cur = await db.execute("""SELECT channel_id FROM logs WHERE guild_id=?""", (interaction.guild.id, ))
+
+        logs = await cur.fetchone()
+
+        chan = discord.utils.get(interaction.guild.text_channels, id=logs[0])
+
+        message_list = []
+        async for message in interaction.channel.history(limit=None):
+            message_list.append(message)
+
+        with open(f"{interaction.channel.id}_{interaction.user}.txt", "a" ,encoding="utf=8") as f:
+            for message in reversed(message_list):
+                if message.embeds:
+                    if message.embeds[0].title:
+                        f.write(f"TITLE - {message.embeds[0].title}\n")
+                    if message.embeds[0].description:
+                        f.write(f"DESCRIPTION - {message.embeds[0].description}\n")
+                    if len(message.embeds[0].fields) > 0:
+                        for field in message.embeds[0].fields:
+                            f.write(f"FIELD - {field.name} - {field.value}\n")
+                    if message.embeds[0].footer:
+                        f.write(f"FOOTER - {message.embeds[0].footer.text}\n")
+                    if message.embeds[0].image:
+                        f.write(f"IMAGE - {message.embeds[0].image.url}\n")
+                    if message.embeds[0].thumbnail:
+                        f.write(f"THUMBNAIL - {message.embeds[0].thumbnail.url}\n")
+                    if message.embeds[0].author:
+                        f.write(f"AUTHOR - {message.embeds[0].author.name}\n")
+                    if message.embeds[0].url:
+                        f.write(f"LINK - {message.embeds[0].url}\n")
+                    f.write(f"LINK - {message.jump_url}\n\n")
+                    continue
+                f.write(f"{message.author}: {message.content}\n")
+        await chan.send(file=discord.File(f"{interaction.channel.id}_{interaction.user}.txt"))
+        os.remove(f"{interaction.channel.id}_{interaction.user}.txt")
+
+        await interaction.followup.send(embed=discord.Embed(description=f"Transcript sent to <#{chan.id}> by {interaction.user.mention}. 📑",color=Color.blurple()))
+
+
+
+
+
+class Ticketing(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
+
+    @app_commands.command(name='tickets', description='Sets the ticket channel.')
+    @app_commands.describe(channel='The channel the open ticket button will be in', ticket_category='The category opened tickets will be in', ticket_banned='A role that bans users from creating tickets.', auto_transcript='Automatically sends the ticket transcript to the logs channel.')
+    @app_commands.default_permissions(administrator=True)
+    async def createticket(self, interaction:discord.Interaction, channel:discord.TextChannel,ticket_category:discord.CategoryChannel,ticket_banned:discord.Role, auto_transcript:bool=False):
+        await interaction.response.defer(ephemeral=True,thinking=True)
+        db = await aiosqlite.connect("ticketing.db")
+        await db.execute("""CREATE TABLE IF NOT EXISTS ticketing(guild_name STRING, guild_id INTEGER, ticket_category_id INTEGER, ticket_channel_id INTEGER,ticket_banned INTEGER,auto_transcript STRING)""")
+        cur = await db.execute("""SELECT * FROM ticketing WHERE guild_id = ?""", (interaction.guild.id, ))
+        row = await cur.fetchone()
+        if row is None:
+            await db.execute("""INSERT INTO ticketing(guild_name, guild_id, ticket_category_id, ticket_channel_id,ticket_banned ,auto_transcript) VALUES (?,?,?,?,?,?)""", (interaction.guild.name, interaction.guild.id, ticket_category.id, channel.id,ticket_banned.id, str(auto_transcript), ))
+            await db.commit()
+            await interaction.followup.send(f'{interaction.user.mention}, set "{ticket_category}" as ticket category <#{channel.id}> as the ticket channel and <@&{ticket_banned.id}> as the banned role!')
+            await channel.send(embed=discord.Embed(
+                title='**Blacklist Appeals**',
+                description='Click the button below to make a blacklist appeal.',color=Color.green(), 
+            ).set_footer(value='Border hopping hispanic - Ban appeals for free'),view=CreateButton())
+        else:
+            await db.execute("""UPDATE ticketing SET ticket_category_id = ?, ticket_channel_id = ?,ticket_banned=? ,auto_transcript = ? WHERE guild_id = ?""", (ticket_category.id,channel.id,ticket_banned.id,str(auto_transcript), interaction.guild.id, ))
+            await db.commit()
+            await interaction.followup.send(f'{interaction.user.id}, set "{ticket_category}" as ticket category, <#{channel.id}> as the ticket channel and <@&{ticket_banned.id}> as the banned role!')
+            await channel.send(embed=discord.Embed(
+                title='**Blacklist Appeals**',
+                description='Click the button below to make a blacklist appeal.',color=Color.green()
+            ),view=CreateButton())
+async def setup(bot):
+    await bot.add_cog(Ticketing(bot))
