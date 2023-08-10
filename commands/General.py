d404e3d4a9781fc5cd6049d39dcfd2024875baed
Ramzina <136939704+Ramzina@users.noreply.github.com>
2023-08-10 12:10:07 +0100

    Update General.py

--- commands/General.py
+++ commands/General.py
@@ -22,7 +22,7 @@ class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-        
+		
 
 ####################################################################################################################################
 #################################################################################################################################### ECHO START
@@ -57,13 +57,7 @@ class General(commands.Cog):
 		embed = discord.Embed(
 			title="! **ECHO** !", description=f"- 🧍 User: {interaction.user.mention}\n- 🪪 User name: {interaction.user}\n- 🪪 User Id: `{interaction.user.id}`\n- 🖊️ Message: {message}",timestamp=datetime.utcnow(),color=Color.from_rgb(27, 152, 250)
 		)
-		if logs is not None:
-			await channel.send(
-				embed=embed.set_thumbnail(url=interaction.user.avatar.url)
-			)
-		else:
-			pass
-
+		if logs is not None: await channel.send(embed=embed.set_thumbnail(url=interaction.user.avatar.url))
 	@echo.error
 	async def on_echo_error(
 		self, interaction: discord.Interaction, error: AppCommandError
@@ -83,31 +77,17 @@ class General(commands.Cog):
 		self, interaction: discord.Interaction, member: discord.Member
 	) -> None:
 		print("[Kys] has just been executed")
-		await interaction.response.defer(ephemeral=True, thinking=True)
+		await interaction.response.defer(thinking=True)
 
-		if member.mention == interaction.user.mention:
-			embed = discord.Embed(title="! Error !",timestamp=datetime.utcnow(), color=Color.from_rgb(27, 152, 250))
-			embed.add_field(name="Command:", value=f"`{interaction.command}`")
-			embed.add_field(
-				name="Error:", value="`Passed member is the same as the message author`"
-			)
-			await interaction.followup.send(
-				embed=embed.set_thumbnail(url=interaction.user.avatar_url)
-			)
+		if member.id == interaction.user.id:
+			await interaction.followup.send(embed=discord.Embed(title="**Kys error**",description=f'> Error: `Member is same as executing user`', color=Color.red()))
 			return
 		embed = discord.Embed(
-			title="! **• KYS ALERT •** !",
-			description=f"\n- Alert {member.mention}, {interaction.user.mention} thinks you should impale yourself on a railing!\n:O\n",
+			title="**Kys**",
+			description=f"\n> Alert: {member.mention}, {interaction.user.mention} thinks you should carve your wrists!:O",
 			timestamp=datetime.utcnow(),
-			color=Color.from_rgb(27, 152, 250),
-		)
-
-		embed.set_footer(text=f"Border hopping hispanic")
-		await interaction.response.send_message(
-			content=f"||{member.mention}||",
-			embed=embed.set_thumbnail(url=interaction.user.avatar.url),
-		)
-
+			color=Color.green())
+		await interaction.followup.send(embed=embed)
 	@kys.error
 	async def on_kys_error(
 		self, interaction: discord.Interaction, error: AppCommandError
@@ -162,19 +142,11 @@ class General(commands.Cog):
 	)
 	@app_commands.checks.cooldown(1, 10, key=lambda i: (i.guild_id, i.user.id))
 	async def random(
-		self, interaction: discord.Interaction, num1: int = 1, num2: int = 100
-	):
-		await interaction.response.defer(ephemeral=True, thinking=True)
+		self, interaction: discord.Interaction, num1: int = 1, num2: int = 100):
+
 		print("[Random] has just been executed")
 
-		embed = discord.Embed(
-			title="**Random**",
-			description=f"> Number: {random.randint(num1, num2)}",
-			color=Color.green(),
-		)
-		await interaction.followup.send(
-			embed=embed, ephemeral=True
-		)
+		await interaction.response.send_message(embed=discord.Embed(title="**Random**",description=f"> Number: {random.randint(num1, num2)}",color=Color.green()))
 
 	@random.error
 	async def on_random_error(
@@ -189,20 +161,17 @@ class General(commands.Cog):
 
 	@app_commands.command(name='weather', description='Gets the forecast in the selected city.')
 	@app_commands.checks.cooldown(1,15, key=lambda i: (i.guild.id, i.user.id))
-	async def weather(self, interaction:discord.Interaction, city:str):
-		await interaction.response.defer(ephemeral=True, thinking=True)
+	async def weather(self, interaction:discord.Interaction, city:str, private:bool=False):
+		await interaction.response.defer(ephemeral=private, thinking=True)
 		print('[Weather] has just been executed.')
 		async with python_weather.Client(unit=python_weather.METRIC) as client:
 			weather = await client.get(city)
 			embed = discord.Embed(title=f'**Weather in {city.capitalize()}**', description=f"> ☁️ Weather: {weather.current.description}\n> 🌥️ Temperature: {weather.current.temperature}C°\n> 💦 Humidity: {weather.current.humidity}%\n> 🚩 Wind speed: {weather.current.wind_speed} Km/h\n> 🚩 Wind direction: {weather.current.wind_direction}",
 			color=Color.blue())
-			await interaction.followup.send(embed=embed)
+			await interaction.followup.send(embed= discord.Embed(title=f'**Weather in {city.capitalize()}**', description=f"> ☁️ Weather: {weather.current.description}\n> 🌥️ Temperature: {weather.current.temperature}C°\n> 💦 Humidity: {weather.current.humidity}%\n> 🚩 Wind speed: {weather.current.wind_speed} Km/h\n> 🚩 Wind direction: {weather.current.wind_direction}",color=Color.blue()))
 	@weather.error
-	async def on_weather_error(
-		self, interaction: discord.Interaction, error: AppCommandError
-	):
-		if isinstance(error, app_commands.CommandOnCooldown):
-			await interaction.response.send_message(content=str(error), ephemeral=True)
+	async def on_weather_error(self, interaction: discord.Interaction, error: AppCommandError):
+		if isinstance(error, app_commands.CommandOnCooldown): await interaction.response.send_message(embed=discord.Embed(title='**Weather error**', description=f'> Error: {str(error)}'), ephemeral=True)
 
 
 ####################################################################################################################################
@@ -215,13 +184,12 @@ class General(commands.Cog):
 		await interaction.response.defer(ephemeral=True, thinking=True)
 		print('[8ball] has just been executed.')
 
-		possible_answers = ['Yes.', 'No.', 'Possibly.', 'Maybe.', 'The stars tell me yes.', 'The stars tell me no.', 'The stars are unsure.', 'The stars don\'t know yet.']
+		possible_answers = ['Yes.', 'No.', 'Possibly.', 'Maybe.', 'The stars tell me yes.', 'The stars tell me no.', 'The stars are unsure.', 'The stars don\'t know yet.','Fuck it sure.', 'Why not.', 'Sure', 'No fuck off.', 'Lmao never.', 'Always.', 'I don\'t fucking know.', 'YEP', '10000%', 'I think so ¯\_(ツ)_/¯']
 
 		answer = random.choice(possible_answers)
 
-		embed = discord.Embed(title='**The magic 8ball**', description=f'- {interaction.user.mention} asked: "{question}"\n- Magic 8ball says: "{answer}"', color=Color.from_rgb(27, 152, 250))
 
-		await interaction.followup.send(embed=embed.set_thumbnail(url=interaction.user.avatar.url))
+		await interaction.followup.send(embed=discord.Embed(title='**The magic 8ball**', description=f'- {interaction.user.mention} asked: "{question}"\n- Magic 8ball says: "{answer}"', color=Color.green()))
 
 ####################################################################################################################################
 #################################################################################################################################### 8BALL END
