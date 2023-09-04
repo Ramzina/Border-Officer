fa7c8a9fad377d75980b5f862bf370e77ed85934
Ramzina <136939704+Ramzina@users.noreply.github.com>
2023-09-04 20:14:59 +0100

    Update Help.py

--- commands/Help.py
+++ commands/Help.py
@@ -1,4 +1,5 @@
 import discord
+import asyncio
 import aiosqlite
 from discord import app_commands
 from discord.ext import commands
@@ -9,12 +10,13 @@ from discord import Color
 class HelpSelect(Select):
     def __init__(self, bot: commands.Bot):
         super().__init__(
-            placeholder="Choose a category",
+            placeholder="Choose a command category",
             options=[
-                discord.SelectOption(
-                    label=cog_name, description=cog.__doc__
-                ) for cog_name, cog in bot.cogs.items() if cog.__cog_app_commands__ and cog_name not in ['HelpSelect', 'Help']
-            ]
+                discord.SelectOption(label=cog_name, description=cog.__doc__)
+                for cog_name, cog in bot.cogs.items()
+                if cog.__cog_commands__ or cog.__cog_app_commands__
+                and cog_name not in ["HelpSelect", "Help"]
+            ],
         )
 
         self.bot = bot
@@ -26,12 +28,19 @@ class HelpSelect(Select):
         commands_mixer = []
         for i in cog.walk_app_commands():
             commands_mixer.append(i)
-        
+        for i in cog.walk_commands():
+            commands_mixer.append(i)
+
         embed = discord.Embed(
             title=f"{cog.__cog_name__} Commands",
             description="\n".join(
-                f"**/{command.name}:** `{command.description}`"
-                for command in commands_mixer),timestamp=datetime.utcnow(),color=Color.from_rgb(27, 152, 250)).set_footer(text="Border hopping hispanic")
+                f"**{self.bot.command_prefix}{command.name}:** `{command.description}`"
+                for command in commands_mixer
+            ),
+            color=Color.blurple(),
+        ).set_footer(
+            text=f"{len(commands_mixer)} {cog.__cog_name__} Commands"
+        )
 
         view = View().add_item(HelpSelect(self.bot))
         await interaction.response.edit_message(embed=embed, view=view)
@@ -40,19 +49,18 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="help", description="Get the basic help for commands")
-    async def help(self, interaction: discord.Interaction):
-
+    @commands.hybrid_command(name="help", description="Get the basic help for commands")
+    async def help(self, ctx):
         embed = discord.Embed(
             title="Help Command",
             description="Choose a slash command category you need help for",
-            timestamp=datetime.utcnow(),
-            color=Color.from_rgb(27, 152, 250),
-        )
+            color=Color.blurple(),
+        ).set_footer(text='This will be deleted in 30 seconds.')
 
-        view = View().add_item(HelpSelect(self.bot))
-        await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
-
+        view = View().add_item(HelpSelect(self.bot))    
+        msg = await ctx.send(embed=embed, view=view)
+        await asyncio.sleep(30)
+        await msg.delete()
 
 async def setup(bot):
     await bot.add_cog(HelpSelect(bot))
